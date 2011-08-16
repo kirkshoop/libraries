ff7146b9c709cb0e074db6b3b78089313e7303b0
Kirk Shoop <kirk.shoop@gmail.com>
2011-08-15 23:12:14 -0700

    commit unique_hresult and libraries include structure changes

---
+++ unique_hresult.h
@@ -0,0 +1,29 @@
+
+#ifndef UNIQUE_HRESULT_SOURCE
+#define UNIQUE_HRESULT_SOURCE
+
+namespace unique_hresult_def
+{
+	struct tag {};
+
+	inline bool unique_error_ok(HRESULT hr, tag&&)
+	{
+		return SUCCEEDED(hr);
+	}
+
+	inline HRESULT unique_error_default(tag&&)
+	{
+		return S_OK;
+	}
+}
+
+typedef
+	UNIQUE_ERROR_NAMESPACE::unique_error<unique_hresult_def::tag>
+unique_hresult;
+
+inline unique_hresult::static_error hresult_cast(HRESULT raw)
+{
+	return unique_hresult::cast(raw);
+}
+
+#endif // UNIQUE_HRESULT_SOURCE
